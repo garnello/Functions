62a8a900b7985207462891b043ebdc04a67ad736
garnello <yarbashalilov@icloud.com>
2024-04-12 13:04:12 +0700

    Functions for find average

---
+++ Func/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Practical_git;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 3, 6, 9, 12, 15 };
+        
+        double average = Average(numbers);
+        Console.WriteLine("Среднее значение: " + average);
+    }
+
+    static double Average(int[] array)
+    {
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+
+}
